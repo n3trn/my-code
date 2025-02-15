94ecef5a9027825ea376378e7c94b6e1a671330e
nastaran <nasfta@gail.com>
2025-02-15 23:58:34 +0330

    -

---
+++ while-mosalas.py
@@ -0,0 +1,15 @@
+# row = 1
+# n = int(input("rows: "))
+# while n:
+#     print("*" * row)
+#     row+=1
+
+
+print("*****************************")
+
+
+row = 1
+n = int(input("rows: "))
+while n >=row:
+    print("*" * n)
+    n-=1
