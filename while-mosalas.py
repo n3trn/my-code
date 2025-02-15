49a660b1ce7564ca869ec76c3d4127195a7c01d2
nastaran <nasfta@gail.com>
2025-02-15 22:40:42 +0330

    add code for mosalase barax

--- while-mosalas.py
+++ while-mosalas.py
@@ -1,5 +1,15 @@
+# row = 1
+# n = int(input("rows: "))
+# while n:
+#     print("*" * row)
+#     row+=1
+
+
+print("*****************************")
+
+
 row = 1
 n = int(input("rows: "))
-while row <= n:
-    print("*" * row)
-    row+=1
+while n >=row:
+    print("*" * n)
+    n-=1
