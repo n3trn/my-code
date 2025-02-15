2ad0d47fbc85be85c15621117b00a0f80314eefe
nastaran <nasfta@gail.com>
2025-02-15 20:22:44 +0330

    1

---
+++ list-exercise.py
@@ -0,0 +1,16 @@
+marks =[]
+name =input("name: ")
+marks.append(name)
+
+fizik = float(input("nomre fizizk: "))
+marks.append(fizik)
+
+riazi = float(input("nomre riazi: "))
+marks.append(riazi)
+
+zaban = float(input("nomre zaban: "))
+marks.append(zaban)
+
+print(marks)
+ave =(marks[1] + marks[2] + marks[3])/3
+print(ave)
